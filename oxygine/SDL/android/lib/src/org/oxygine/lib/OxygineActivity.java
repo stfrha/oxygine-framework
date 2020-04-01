4675c9743853338b4ea0f065bb83e92efa2c12a6
stfrha <mail@fredrikhoffman.se>
2020-04-01 20:16:16 +0200

    Changed OxygineActivity.java to handle hiding of navigation pane.

--- oxygine/SDL/android/lib/src/org/oxygine/lib/OxygineActivity.java
+++ oxygine/SDL/android/lib/src/org/oxygine/lib/OxygineActivity.java
@@ -6,6 +6,9 @@ import org.libsdl.app.SDLActivity;
 import org.oxygine.lib.extension.ActivityObservable;
 import org.oxygine.lib.extension.ActivityObserver;
 
+// Below is added by frha from https://stackoverflow.com/questions/41059909/hiding-the-navigation-bar-in-pure-android-native
+import android.view.View;
+
 /**
  * OxygineActivity
  */
@@ -27,14 +30,43 @@ public class OxygineActivity extends SDLActivity {
         return _observable.findClass(c);
     }
 
+
+    // Below is added by frha from https://stackoverflow.com/questions/41059909/hiding-the-navigation-bar-in-pure-android-native
+    void setImmersiveSticky() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Below is added by frha from https://stackoverflow.com/questions/41059909/hiding-the-navigation-bar-in-pure-android-native
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT >= 19) {
+            setImmersiveSticky();
+
+            View decorView = getWindow().getDecorView();
+            decorView.setOnSystemUiVisibilityChangeListener
+                    (new View.OnSystemUiVisibilityChangeListener() {
+                        @Override
+                        public void onSystemUiVisibilityChange(int visibility) {
+                            setImmersiveSticky();
+                        }
+                    });
+        }
+
         super.onCreate(savedInstanceState);
         instance = this;
         Utils._context = this;
         nativeOxygineInit(this, getClass());
 
         _observable.onCreate();
+
     }
 
     @Override
@@ -63,6 +95,18 @@ public class OxygineActivity extends SDLActivity {
 
     @Override
     public void onResume() {
+
+        // Below is added by frha from https://stackoverflow.com/questions/41059909/hiding-the-navigation-bar-in-pure-android-native
+        //Hide toolbar
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT >= 11 && SDK_INT < 14) {
+            getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_HIDDEN);
+        } else if (SDK_INT >= 14 && SDK_INT < 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        } else if (SDK_INT >= 19) {
+            setImmersiveSticky();
+        }
+
         super.onResume();
         _observable.onResume();
     }
